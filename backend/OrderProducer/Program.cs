1ff9942d1bccbee197a9b8beaff6a5f459536c68
Ivan Gonzalez <navi@pop-os.localdomain>
2021-08-30 23:21:04 -0600

    Refactored .NET Core projects to follow microservice structure and implemented a basic Kafka Consumer

---
+++ backend/OrderProducer/Program.cs
@@ -0,0 +1,31 @@
+﻿// Simple example of a producer
+using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace backend
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:29092" };
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("customer-orders", new Message<Null, string> { Value="Hello from .NET" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
